370eb97138c55d117d7d8ac2f5e71d0d05cda55b
unknown <waleed.aptech.use@gmail.com>
2025-08-11 23:04:19 -0700

    all files

---
+++ 1st.js
@@ -0,0 +1,35 @@
+// // let a = 10;
+// // let b = 20;
+
+// // let result = a + b;
+// // document.write(result)
+
+// // LOGICAL OPERATORS
+
+// // let a = 10;
+// // let b = 20;
+
+// // let result = a <= b && a == b;
+// // document.write(result)
+
+// // let num1 = 20;
+// // let num2 = 30;
+// // let num3 = 10;
+
+// // let result = num1 <= num2 && num1 == num2 && num3 > num1;
+// // document.write(result)
+
+// // let num1 = 20;
+// // let num2 = 30;
+// // let num3 = 10;
+
+// // let result = !(num1 <= num2 && num1 != num2 && num3 > num1);
+// // document.write(result)
+
+// //ASSIGMENT OPERATORS
+
+// let num1 = 40;
+// let num2 = 20;
+
+// let result = num1 > num2 ? "TRUE" : "FALSE";
+// document.write(result) 
---
+++ script1.js
@@ -0,0 +1,20 @@
+// let num = prompt("Enter the num")
+
+// if (num > 0) {
+//     document.write(num + " is positive");
+// }
+
+// else{
+//     document.write(num + " is negative");
+// }
+
+
+let num2 = prompt("Enter the num")
+
+if (num2 % 2 == 0) {
+    document.write(num + " IS EVEN");
+}
+
+else{
+    document.write(num2 + " IS ODD");
+}
