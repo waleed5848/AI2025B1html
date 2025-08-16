06d92d3446f36597453ba5e06d47fc0ecf23ca06
unknown <waleed.aptech.use@gmail.com>
2025-08-15 22:49:02 -0700

    all files

--- script1.js
+++ script1.js
@@ -9,12 +9,67 @@
 // }
 
 
-let num2 = prompt("Enter the num")
+// let num2 = prompt("Enter the num")
 
-if (num2 % 2 == 0) {
-    document.write(num + " IS EVEN");
+// if (num2 % 2 == 0) {
+//     document.write(num + " IS EVEN");
+// }
+
+// else{
+//     document.write(num2 + " IS ODD");
+// }
+
+// let language = prompt("Enter the language");
+
+// if(language == "html"){
+//     document.write("HTML is used to give structure to webpage");
+// }
+// else if(language == "JS"){
+//     document.write("JS is a client-side scripting language");
+// }
+// else if(language == "CSS"){
+//     document.write("CSS is used to give style to webpage");
+// }
+// else{
+//     document.write("Invalid language");
+// }
+
+// let num1 = Number(prompt("Enter the num"));
+// let num2 = Number(prompt("Enter the num"));
+
+// let op = prompt("Enter the operator");
+
+// if (op == "+") {
+//     document.write(num1 + " + " + num2 + " = " + (num1 + num2));
+// }
+// else if(op == "-"){
+//     document.write(num1 + " - " + num2 + " = " + (num1 - num2));
+// }
+// else if(op == "*"){
+//     document.write(num1 + " * " + num2 + " = " + (num1 * num2));
+// }
+// else if(op == "/"){
+//     document.write(num1 + " / " + num2 + " = " + (num1 / num2));
+// }
+// else {
+//     document.write("Invalid operator");
+// }
+
+let nationality = prompt("Enter your nationality");
+let age = Number(prompt("Enter your age"));
+
+if(nationality == "pakistani"){
+    if(age >= 18){
+        document.write("You are eligible to vote");
 }
+else{
+    document.write("You are not eligible to vote");
+}
+}
+
 
 else{
-    document.write(num2 + " IS ODD");
+    document.write("only pakistani are eligible to vote");
 }
+
+
