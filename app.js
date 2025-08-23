e646a422bb02c74e22fd22cb16cc31a4f9a577d5
unknown <waleed.aptech.use@gmail.com>
2025-08-22 22:58:42 -0700

    all files

---
+++ app.js
@@ -0,0 +1,24 @@
+// let language = prompt("Enter your Fav language")
+
+// switch(language){
+//     case "Html" :
+//         document.write("html is the backbone of webpage")
+//         break;
+
+//         case "CSS" :
+//         document.write("Css is use to design the webpage")
+//         break;
+
+//         case "JS" :
+//         document.write("JS gives functionality to the webpage")
+//         break;
+
+
+//         default :
+//             document.write("Invalid language!!!")
+//             break;
+// }
+
+// let vow = prompt("Enter tha alphabat")
+// vow =vowel.toLowerCase()
+        
