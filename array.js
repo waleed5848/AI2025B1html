8423ee6c62da1eec7541342c9a7edc7ad0cebaac
unknown <waleed.aptech.use@gmail.com>
2025-08-25 23:03:34 -0700

    all files

---
+++ array.js
@@ -0,0 +1,6 @@
+let student = ["Waleed","ahmed","mustafa","anas"]
+
+document.write(student[2],"&nbsp;",student[1], "<br>")
+
+let courses = new Array("CPISM","DISM","HDSE1","HDSE2")
+document.write(student.indexOf("ahmed"))
