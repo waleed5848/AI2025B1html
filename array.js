edbdd86ee532ce25c28d705baef27c0e544c6c88
unknown <waleed.aptech.use@gmail.com>
2025-08-27 22:13:46 -0700

    all files

--- array.js
+++ array.js
@@ -1,6 +1,8 @@
-let student = ["Waleed","ahmed","mustafa","anas"]
+// let student = ["Waleed","ahmed","mustafa","anas"]
 
-document.write(student[2],"&nbsp;",student[1], "<br>")
+// document.write(student[2],"&nbsp;",student[1], "<br>")
 
-let courses = new Array("CPISM","DISM","HDSE1","HDSE2")
-document.write(student.indexOf("ahmed"))
+// let courses = new Array("CPISM","DISM","HDSE1","HDSE2")
+// document.write(student.indexOf("ahmed"))
+
+
---
+++ multiarray.js
@@ -0,0 +1,11 @@
+let courses = [
+    ["html","css","js","bootstrap","jquery"],
+    ["SQl","XML","PHP","ADV.JS","Json"],
+    ["SQL servor","C#",".Net","Angular"]
+]
+
+for(value of courses){
+    for(data of value){
+
+document.write(data,"<br>")}
+}
