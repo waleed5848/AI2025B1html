85e488f1ecd8c185df9636038b7b55f070c744f5
unknown <waleed.aptech.use@gmail.com>
2025-08-27 23:00:45 -0700

    all files

--- multiarray.js
+++ multiarray.js
@@ -1,11 +1,19 @@
-let courses = [
-    ["html","css","js","bootstrap","jquery"],
-    ["SQl","XML","PHP","ADV.JS","Json"],
-    ["SQL servor","C#",".Net","Angular"]
-]
+// let courses = [
+//     ["html","css","js","bootstrap","jquery"],
+//     ["SQl","XML","PHP","ADV.JS","Json"],
+//     ["SQL servor","C#",".Net","Angular"]
+// ]
 
-for(value of courses){
-    for(data of value){
+// for(value of courses){
+//     for(data of value){
 
-document.write(data,"<br>")}
-}
+// document.write(data,"<br>")}
+// }
+
+let fruit = ["orange","apple","mango"]
+// fruit.push("graps","banana")
+// fruit.pop()
+
+// fruit.unshift("banana")
+// fruit.shift()
+document.write(fruit)
