76b1f44a92de889b5774eb573b788aaedb1e41cb
unknown <waleed.aptech.use@gmail.com>
2025-09-03 22:52:41 -0700

    all files

--- function.js
+++ function.js
@@ -18,12 +18,24 @@
 //     document.write("hello pak")
 // }
 
- let btn = document.getElementById("btn")
+//  let btn = document.getElementById("btn")
 // let para = document.getElementById("para")
 
 // btn.onclick = ()=> para.innerHTML = "hello world";
 // btn.onmouseover = ()=> para.style.color = "red";
 
-btn.addEventListener("click",()=>{
-    document.getElementById("para").innerHTML = "hello world"
-})
+// btn.addEventListener("click",()=>{
+//     document.getElementById("para").innerHTML = "hello world"
+// })
+// window.onload = function(){
+//     alert("page is loaded")
+// }
+// window.onscroll = function(){
+//     alert("page is scrolled")
+// }
+// window.onresize = function(){
+//     alert("page is resized")
+// }
+window.onbeforeunload = function(e){
+e.preventDefault();
+}
---
+++ object.js
@@ -0,0 +1,10 @@
+let students={
+    id : 101,
+    name : "Ali",
+    email : "ali@gmail.com",
+    age : 18
+}
+
+for (value in students){
+    document.write(value , ":" , students[value] , "<br>")
+}
