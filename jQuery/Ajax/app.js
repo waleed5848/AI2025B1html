b92a861f17e5e94de42988ffe6a6ee2d85b02d01
Saleem <waleed.aptech.use@gmail.com>
2025-10-31 22:55:54 -0700

    allfiles

--- jQuery/Ajax/app.js
+++ jQuery/Ajax/app.js
@@ -38,11 +38,12 @@ $(document).ready(function(){
                 user.forEach(users => {
                     myhtml+= `
                     <div class="card" style="width: 18rem;">
-  <img src="${users.p_img}" class="card-img-top" alt="...">
+  <img src="${users.p_img}" class="card-img-top"  alt="...">
   <div class="card-body">
     <h5 class="card-title">${users.p_name}</h5>
+    <h5 class="card-title">${users.p_price}</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">${users.p_price}</a>
+    <a href="#" class="btn btn-primary">View more</a>
   </div>
 </div>`
 
